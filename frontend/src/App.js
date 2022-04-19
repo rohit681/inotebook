9b9323ee88eb93905cfe2c558cfc64d490eb1cf3
rohit681 <rohit.vishnani_cs19@gla.ac.in>
2022-04-19 16:24:58 +0530

    added react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
